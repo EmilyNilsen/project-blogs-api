61739cd0aa6a36c6c91421c7fac295e5d45b145d
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-05-25 10:37:02 -0300

    fix: documentado com o swagger

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
     host: process.env.HOSTNAME,
+    port: process.env.PORTDB,
     dialect: 'mysql',
   },
   test: {
@@ -13,6 +14,7 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
     host: process.env.HOSTNAME,
+    port: process.env.PORTDB,
     dialect: 'mysql',
   },
   production: {
@@ -20,6 +22,7 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
     host: process.env.HOSTNAME,
+    port: process.env.PORTDB,
     dialect: 'mysql',
   },
 };
